b5b6fb78458b5024072ed27a9dcb3ee5e5bc9ba9
yurii <yurii.zadoianchuk@adyen.com>
2022-03-20 18:22:04 +0100

    Update SwiftyBERTLV dependency to a remote one

--- Package.swift
+++ Package.swift
@@ -14,7 +14,11 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "SwiftyBERTLV", path: "../swifty-ber-tlv")
+        .package(
+            name: "SwiftyBERTLV",
+            url: "https://github.com/kaphacius/swifty-ber-tlv",
+            branch: "develop"
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
