c7f3287f047c4a3ca1f7270a332eaf6db94d0feb
Yurii <yurii.zadoianchuk@gmail.com>
2022-09-11 09:15:15 +0200

    Add methods for extracting pattern bits

---
+++ Sources/SwiftyEMVTags/UInt8Extensions.swift
@@ -0,0 +1,48 @@
+//
+//  File.swift
+//  
+//
+//  Created by Yurii Zadoianchuk on 10/09/2022.
+//
+
+import Foundation
+
+internal struct BitExtractionResult {
+    
+    let extracted: UInt8
+    let patternLength: Int
+    
+}
+
+extension UInt8 {
+    
+    init(pattern: String) throws {
+        guard let decoded = UInt8(pattern, radix: 2) else {
+            throw DecodingError.dataCorrupted(
+                .init(
+                    codingPath: [],
+                    debugDescription: "Unable to create UInt8 with pattern \(pattern)"
+                )
+            )
+        }
+        
+        self = decoded
+    }
+    
+    func extractingBits(with pattern: UInt8) -> BitExtractionResult {
+        let shift = pattern.bitWidth - pattern.leadingZeroBitCount - pattern.nonzeroBitCount
+        return .init(
+            extracted: (self & pattern) >> shift,
+            patternLength: pattern.nonzeroBitCount
+        )
+    }
+    
+    func matches(pattern: UInt8) -> Bool {
+        self == pattern
+    }
+    
+    var bitString: String {
+        String(repeating: "0", count: self.leadingZeroBitCount) + String(self, radix: 2)
+    }
+    
+}
---
+++ Tests/SwiftyEMVTagsTests/UInt8ExtensionsTests.swift
@@ -0,0 +1,87 @@
+//
+//  UInt8ExtensionsTests.swift
+//  
+//
+//  Created by Yurii Zadoianchuk on 10/09/2022.
+//
+
+import XCTest
+@testable
+import SwiftyEMVTags
+
+final class UInt8ExtensionsTests: XCTestCase {
+
+    func testBitExtraction() throws {
+        try [
+            ("00110000", "00100000", 2),
+            ("11110000", "10100000", 10),
+            ("11111110", "10000000", 64),
+            ("00000001", "11111110", 0),
+            ("00011100", "11110111", 5),
+        ]
+            .map {
+                try (pattern: UInt8(pattern: $0.0),
+                     byte: UInt8(pattern: $0.1),
+                     expectedResult: UInt8($0.2))
+            }
+            .map { ($0.pattern, $0.byte.extractingBits(with: $0.pattern), $0.expectedResult) }
+            .forEach { arg0 in
+                let (pattern, sut, expectedResult) = arg0
+                XCTAssertEqual(sut.patternLength, pattern.nonzeroBitCount)
+                XCTAssertEqual(sut.extracted, expectedResult)
+            }
+    }
+    
+    func testMatchesPattern() throws {
+        let groupPattern: UInt8 = 0b11000000
+        let bytes = [
+            0b00000000,
+            0b01000000,
+            0b10000000,
+            0b11000000
+        ].map { (byte: UInt8) in
+            byte.extractingBits(with: groupPattern)
+        }.map(\.extracted)
+        
+        let valuePatterns: [UInt8] = [
+            0b00000000,
+            0b00000001,
+            0b00000010,
+            0b00000011
+        ]
+        
+        XCTAssertTrue(
+            zip(bytes, valuePatterns)
+                .map { $0.0.matches(pattern: $0.1) }
+                .allSatisfy { $0 }
+        )
+        
+    }
+    
+    func testDoesntMatchPattern() throws {
+        let groupPattern: UInt8 = 0b11000000
+        let bytes = [
+            0b00000000,
+            0b01000000,
+            0b10000000,
+            0b11000000
+        ].map { (byte: UInt8) in
+            byte.extractingBits(with: groupPattern)
+        }.map(\.extracted)
+        
+        let valuePatterns: [UInt8] = [
+            0b00000001,
+            0b00000000,
+            0b00000011,
+            0b00000001
+        ]
+        
+        XCTAssertTrue(
+            zip(bytes, valuePatterns)
+                .map { $0.0.matches(pattern: $0.1) }
+                .allSatisfy { $0 == false }
+        )
+        
+    }
+
+}
