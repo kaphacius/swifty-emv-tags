65c6da4ed438d31b3ff5c26d506e9a3e83010595
Yurii <yurii.zadoianchuk@gmail.com>
2022-12-17 16:18:08 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(
             name: "SwiftyBERTLV",
             url: "https://github.com/kaphacius/swifty-ber-tlv",
-            from: .init(stringLiteral: "0.4.0")
+            from: .init(stringLiteral: "0.4.2")
         )
     ],
     targets: [
