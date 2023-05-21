178c6392b745e48363f0c6963e35e0123390f6aa
Yurii <yurii.zadoianchuk@gmail.com>
2023-05-21 08:53:10 +0200

    Bump dependency version

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(
             name: "SwiftyBERTLV",
             url: "https://github.com/kaphacius/swifty-ber-tlv",
-            from: .init(stringLiteral: "0.4.2")
+            from: .init(stringLiteral: "0.4.3")
         )
     ],
     targets: [
